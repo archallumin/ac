d70e9aa2618d0333e08b961cdd8872b0daab297e
allumin <windowsfilesystem@gmail.com>
2024-01-28 16:02:06 +0900

    aaaaa

--- 20001.cpp
+++ 20001.cpp
@@ -1,2 +1,26 @@
 #include <iostream>
 using namespace std;
+
+int main(void) {
+  int problem = 0;
+  string a, b;
+  cin >> a;
+  while (b != "고무오리 디버깅 끝") {
+    cin >> b;
+    if (b == "문제") {
+      problem++;
+    }
+    if (b == "고무오리") {
+      if (problem == 0) {
+        problem += 2;
+      } else
+        problem--;
+    }
+  }
+  if (problem)
+    cout << "고무오리야 사랑해\n";
+  else
+    cout << "힝구\n";
+  return 0;
+}
+// why?????
