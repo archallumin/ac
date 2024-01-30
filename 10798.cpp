a44fd3e534a53ef282eb3b35e905bbb92fba1a8a
allumin <windowsfilesystem@gmail.com>
2024-01-30 09:54:58 +0900

    m

--- 10798.cpp
+++ 10798.cpp
@@ -3,20 +3,16 @@ using namespace std;
 
 char arr[5][15];
 
-int main(void)
-{
-    for (int i = 0; i < 5; i++)
-    {
-        cin >> arr[i];
+int main(void) {
+  for (int i = 0; i < 5; i++) {
+    cin >> arr[i];
+  }
+  for (int j = 0; j < 15; j++) {
+    for (int k = 0; k < 5; k++) {
+      if (arr[k][j] != NULL)
+        cout << arr[k][j];
     }
-    for (int j = 0; j < 15; j++)
-    {
-        for (int k = 0; k < 5; k++)
-        {
-            if (arr[k][j] != NULL)
-                cout << arr[k][j];
-        }
-    }
-    cout << "\n";
-    return 0;
+  }
+  cout << "\n";
+  return 0;
 }
