28a84779bdd961e65ddc192c42c957671f41b172
allumin <windowsfilesystem@gmail.com>
2024-01-30 14:36:28 +0900

    14681

--- 14681.cpp
+++ 14681.cpp
@@ -2,12 +2,16 @@
 using namespace std;
 
 int main(void) {
-	int a, b;
-	cin >> a;
-	cin >> b;
-	if (a > 0 && b > 0) cout << "1\n";
-	else if (a < 0 && b > 0) cout << "2\n";
-	else if (a < 0 && b < 0) cout << "3\n";
-	else if (a > 0 && b < 0) cout << "4\n";
-	return 0;
+  int a, b;
+  cin >> a;
+  cin >> b;
+  if (a > 0 && b > 0)
+    cout << "1\n";
+  else if (a < 0 && b > 0)
+    cout << "2\n";
+  else if (a < 0 && b < 0)
+    cout << "3\n";
+  else if (a > 0 && b < 0)
+    cout << "4\n";
+  return 0;
 }
