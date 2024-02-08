652c2738a702da2b1b28e9e6a1b0e198dc2e3dfe
allumin <windowsfilesystem@gmail.com>
2024-02-08 13:41:12 +0900

    1012!

--- 1012.cpp
+++ 1012.cpp
@@ -1,5 +1,6 @@
 // 그래프를 더 연습해야겠다!
 
+#include <cstring>
 #include <iostream>
 using namespace std;
 
@@ -11,13 +12,11 @@ bool isvisited[51][51];
 int search_x[4] = {0, 0, 1, -1};
 int search_y[4] = {1, -1, 0, 0};
 
-int dfs(int y, int x)
-{
+int dfs(int y, int x) {
   if (isvisited[y][x])
     return 0;
   isvisited[y][x] = true;
-  for (int i = 0; i < 4; i++)
-  {
+  for (int i = 0; i < 4; i++) {
     int xx = x + search_x[i];
     int yy = y + search_y[i];
     if (xx >= 0 && yy >= 0 && xx < a && yy < b && f[yy][xx])
@@ -26,30 +25,24 @@ int dfs(int y, int x)
   return 1;
 }
 
-int main(void)
-{
+int main(void) {
   cin.tie(0);
   ios::sync_with_stdio(false);
   int t;
   cin >> t;
-  while (t--)
-  {
+  while (t--) {
     cin >> a >> b >> c;
     memset(f, 0, sizeof(f));
     memset(isvisited, 0, sizeof(isvisited));
-    for (int i = 0; i < c; i++)
-    {
+    for (int i = 0; i < c; i++) {
       int x, y;
       cin >> x >> y;
       f[y][x] = 1;
     }
     int cnt = 0;
-    for (int j = 0; j < b; j++)
-    {
-      for (int k = 0; k < a; k++)
-      {
-        if (f[j][k] && !isvisited[j][k])
-        {
+    for (int j = 0; j < b; j++) {
+      for (int k = 0; k < a; k++) {
+        if (f[j][k] && !isvisited[j][k]) {
           if (dfs(j, k))
             cnt++;
         }
