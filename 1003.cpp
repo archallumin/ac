8b75adee8a89a376ed68a346eb86cc9cd58b516b
allumin <windowsfilesystem@gmail.com>
2024-02-11 12:02:02 +0900

    1003 edit?

--- 1003.cpp
+++ 1003.cpp
@@ -5,21 +5,18 @@ using namespace std;
 
 long long fib[41];
 
-int main(void)
-{
+int main(void) {
   cin.tie(0);
   ios::sync_with_stdio(false);
   fib[1] = 1;
   fib[2] = 1;
-  for (int i = 3; i < 41; i++)
-  {
+  for (int i = 3; i < 41; i++) {
     fib[i] = fib[i - 1] + fib[i - 2];
   }
   int a;
   int t;
   cin >> t;
-  while (t--)
-  {
+  while (t--) {
     cin >> a;
     if (a == 0)
       cout << "1 0\n";
