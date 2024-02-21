17780256a95800d272d2e12f1f8f4b8b06b46f2d
allumin <windowsfilesystem@gmail.com>
2024-02-21 11:56:41 +0900

    NO!

--- 1259.cpp
+++ 1259.cpp
@@ -1,21 +1,19 @@
-#include <iostream>
 #include <algorithm>
+#include <iostream>
 using namespace std;
 
-int main(void)
-{
-	string a, b;
-	while (1)
-	{
-		cin >> a;
-		if (a == "0")
-			break;
-		b = a;
-		reverse(a.begin(), a.end());
-		if (a == b)
-			cout << "yes\n";
-		else
-			cout << "no\n";
-	}
-	return 0;
+int main(void) {
+  string a, b;
+  while (1) {
+    cin >> a;
+    if (a == "0")
+      break;
+    b = a;
+    reverse(a.begin(), a.end());
+    if (a == b)
+      cout << "yes\n";
+    else
+      cout << "no\n";
+  }
+  return 0;
 }
--- 1926.cpp
+++ 1926.cpp
@@ -13,16 +13,13 @@ vector<int> ps;
 int X[4] = {0, 0, 1, -1};
 int Y[4] = {1, -1, 0, 0};
 
-void dfs(int y, int x)
-{
-  for (int i = 0; i < 4; i++)
-  {
+void dfs(int y, int x) {
+  for (int i = 0; i < 4; i++) {
     int xx = x + X[i];
     int yy = y + Y[i];
     if (xx < 0 || yy < 0 || xx >= b || yy >= a)
       continue;
-    if (!vis[yy][xx] && pic[yy][xx])
-    {
+    if (!vis[yy][xx] && pic[yy][xx]) {
       vis[yy][xx] = 1;
       ttp++;
       dfs(yy, xx);
@@ -30,25 +27,19 @@ void dfs(int y, int x)
   }
 }
 
-int main(void)
-{
+int main(void) {
   cin.tie(0);
   ios::sync_with_stdio(false);
   int tp = 0;
   cin >> a >> b;
-  for (int i = 0; i < a; i++)
-  {
-    for (int j = 0; j < b; j++)
-    {
+  for (int i = 0; i < a; i++) {
+    for (int j = 0; j < b; j++) {
       cin >> pic[i][j];
     }
   }
-  for (int k = 0; k < a; k++)
-  {
-    for (int l = 0; l < b; l++)
-    {
-      if (pic[k][l] == 1 && !vis[k][l])
-      {
+  for (int k = 0; k < a; k++) {
+    for (int l = 0; l < b; l++) {
+      if (pic[k][l] == 1 && !vis[k][l]) {
         vis[k][l] = 1;
         ttp = 1;
         dfs(k, l);
@@ -58,13 +49,10 @@ int main(void)
     }
   }
   cout << tp << "\n";
-  if (ps.size() == 0)
-  {
+  if (ps.size() == 0) {
     cout << "0\n";
     return 0;
-  }
-  else
-  {
+  } else {
     sort(ps.begin(), ps.end(), greater<int>());
     cout << ps[0] << "\n";
     return 0;
