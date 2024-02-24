fed63e9e9a0475074122f4110a6b02f9e59c44ec
allumin <windowsfilesystem@gmail.com>
2024-02-24 18:47:16 +0900

    derftgyhujiok

--- 1000 - 5000/2525.cpp
+++ 1000 - 5000/2525.cpp
@@ -2,17 +2,17 @@
 using namespace std;
 
 int main(void) {
-	int h, m, t;
-	cin >> h >> m;
-	cin >> t;
-	m = m + t;
-	while (m >= 60) {
-		m = m - 60;
-		h = h + 1;
-		if (h >= 24) {
-			h = 0;
-		}
-	}
-	cout << h << " " << m << "\n";
-	return 0;
+  int h, m, t;
+  cin >> h >> m;
+  cin >> t;
+  m = m + t;
+  while (m >= 60) {
+    m = m - 60;
+    h = h + 1;
+    if (h >= 24) {
+      h = 0;
+    }
+  }
+  cout << h << " " << m << "\n";
+  return 0;
 }
