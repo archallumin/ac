9bcecc236825d33aa6caccad0dd4af3ee1f6a031
allumin <windowsfilesystem@gmail.com>
2024-02-26 08:48:39 +0900

    mkkmmk

--- 20001 - 30000/25314.cpp
+++ 20001 - 30000/25314.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 
 int main(void) {
-	int a;
-	cin >> a;
-	while (a > 0) {
-		cout << "long ";
-		a = a - 4;
-	}
-	cout << "int\n";
-	return 0;
+  int a;
+  cin >> a;
+  while (a > 0) {
+    cout << "long ";
+    a = a - 4;
+  }
+  cout << "int\n";
+  return 0;
 }
