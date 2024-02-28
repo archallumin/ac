bdd5770ebc4b7fd7b19ffdd40028282c89ec39f4
allumin <windowsfilesystem@gmail.com>
2024-02-29 00:18:57 +0900

    GONEFPNEOIDFO

--- 5001 - 10000/9086.cpp
+++ 5001 - 10000/9086.cpp
@@ -2,15 +2,13 @@
 #include <string>
 using namespace std;
 
-int main(void)
-{
-	int t;
-	string a;
-	cin >> t;
-	while (t--)
-	{
-		cin >> a;
-		cout << a[0] << a[a.length() - 1] << "\n";
-	}
-	return 0;
+int main(void) {
+  int t;
+  string a;
+  cin >> t;
+  while (t--) {
+    cin >> a;
+    cout << a[0] << a[a.length() - 1] << "\n";
+  }
+  return 0;
 }
