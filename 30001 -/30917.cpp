4b202a82b700991fa28a9034d1a900177f3385ba
allumin <windowsfilesystem@gmail.com>
2024-02-29 08:08:16 +0900

    soooooo busyyyy

--- 30001 -/30917.cpp
+++ 30001 -/30917.cpp
@@ -3,31 +3,26 @@
 #include <iostream>
 using namespace std;
 
-int main(void)
-{
-    int resp;
-    int a, b;
-    for (int i = 1; i <= 9; i++)
-    {
-        cout << "? A " << i << endl;
-        cin >> resp;
-        if (resp == 1)
-        {
-            a = i;
-            break;
-        }
+int main(void) {
+  int resp;
+  int a, b;
+  for (int i = 1; i <= 9; i++) {
+    cout << "? A " << i << endl;
+    cin >> resp;
+    if (resp == 1) {
+      a = i;
+      break;
     }
-    resp = 0;
-    for (int j = 1; j <= 9; j++)
-    {
-        cout << "? B " << j << endl;
-        cin >> resp;
-        if (resp == 1)
-        {
-            b = j;
-            break;
-        }
+  }
+  resp = 0;
+  for (int j = 1; j <= 9; j++) {
+    cout << "? B " << j << endl;
+    cin >> resp;
+    if (resp == 1) {
+      b = j;
+      break;
     }
-    cout << "! " << a + b << "\n";
-    return 0;
+  }
+  cout << "! " << a + b << "\n";
+  return 0;
 }
