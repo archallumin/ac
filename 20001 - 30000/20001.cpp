1331e6d1378f79f07ace556f9dcaddce8f47c3ad
allumin <windowsfilesystem@gmail.com>
2024-02-29 00:13:37 +0900

    goooo nnnn

--- 20001 - 30000/20001.cpp
+++ 20001 - 30000/20001.cpp
@@ -4,23 +4,22 @@ using namespace std;
 int main(void) {
   int problem = 0;
   string a, b;
-  cin >> a;
+  getline(cin, a);
   while (b != "고무오리 디버깅 끝") {
-    cin >> b;
+    getline(cin, b);
     if (b == "문제") {
-      problem++;
+      problem += 1;
     }
     if (b == "고무오리") {
       if (problem == 0) {
         problem += 2;
       } else
-        problem--;
+        problem -= 1;
     }
   }
-  if (problem)
+  if (!problem)
     cout << "고무오리야 사랑해\n";
   else
     cout << "힝구\n";
   return 0;
 }
-// why?????
