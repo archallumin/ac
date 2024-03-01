588901e2d3860256d8e19445aeda037fa47710c0
allumin <windowsfilesystem@gmail.com>
2024-03-01 09:16:18 +0900

    mdkedekm

--- 1000 - 5000/2440.cpp
+++ 1000 - 5000/2440.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 int main(void) {
-	int n;
-	cin >> n;
-	for (int a = 0; a < n; a++) {
-		for (int b = n; b > a; b--) {
-			cout << "*";
-		}
-		cout << "\n";
-	}
-	return 0;
+  int n;
+  cin >> n;
+  for (int a = 0; a < n; a++) {
+    for (int b = n; b > a; b--) {
+      cout << "*";
+    }
+    cout << "\n";
+  }
+  return 0;
 }
