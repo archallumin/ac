995cd3a4729f53e0dddaa2c1510add58bf030cca
allumin <windowsfilesystem@gmail.com>
2024-03-18 08:52:29 +0900

    dkkdekoedokdo

--- others/9wdwe.py
+++ others/9wdwe.py
@@ -1,16 +1,1 @@
-import string
-
-tmp = string.digits + string.ascii_uppercase
-
-
-def convert(num, base):
-    q, r = divmod(num, base)
-    if q == 0:
-        return tmp[r]
-    else:
-        return convert(q, base) + tmp[r]
-
-
-for i in range(500001):
-    print(i, end=" ")
-    print(convert(i, 36))
+# a
