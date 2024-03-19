9574dc319c50185ba0b337d5384926947862a939
allumin <windowsfilesystem@gmail.com>
2024-03-19 22:41:02 +0900

    ijfririjr

--- others/myh.py
+++ others/myh.py
@@ -1,4 +1,20 @@
-a, b, c = "abc"
-for i in range(20):
-    a, b, c = b, c, a + b + c
-print(c)
+import time
+
+seed = int(time.time() * 1000)
+
+
+def pseudo_random():
+    global seed
+    a = 1103515245
+    c = 12345
+    m = 2**31
+    seed = (a * seed + c) % m
+    return seed
+
+
+def random_int(min_val, max_val):
+    return min_val + pseudo_random() % (max_val - min_val + 1)
+
+
+for i in range(6):
+    print(random_int(1, 45))
