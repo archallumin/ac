776726efc63279cb5c46c5b65d2430c0322d0892
allumin <windowsfilesystem@gmail.com>
2024-03-20 16:08:11 +0900

    lkjsdflkjsdfjl

--- others/tt.py
+++ others/tt.py
@@ -1,9 +1,7 @@
-import decimal
-
-decimal.getcontext().prec = 1000
-a = int(input())
-
-for i in range(a):
-    x, y, z, w = map(str, input().split())
-    d = f"{((decimal.getcontext().power(decimal.Decimal(str(x)), decimal.Decimal('0.5')) * decimal.Decimal('0.055')+ decimal.Decimal(str(y))) * decimal.Decimal(str(z)) * (decimal.Decimal('1') / decimal.Decimal(str(w)))):.400f}"
-    print(d[:-350])
+import string
+import random
+for i in range(10):
+  _LENGTH: int = random.randint(5, 5)
+  exam_string: str = string.digits
+  result: str = ''.join(random.choices(exam_string,k=_LENGTH))
+  print(result)
