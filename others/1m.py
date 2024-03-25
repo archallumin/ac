6cc7c98c8e8b26e7dbc2abacdb68c6d8b0f97de5
allumin <windowsfilesystem@gmail.com>
2024-03-26 01:21:21 +0900

    3469

--- others/1m.py
+++ others/1m.py
@@ -1,3 +1,25 @@
-import sys
-sys.set_int_max_str_digits(50000)
-print((2**19937)-1)
+P = lambda s: print((" " * 500 + s)[-l:])
+for _ in [0] * int(input()):
+    s = input()
+    for j in "+-*":
+        if j in s:
+            break
+    p, q = s.split(j)
+    e = eval(s)
+    l = max(len(p), len(q) + 1, len(str(e)))
+    P(p)
+    P(j + q)
+    if j == "*":
+        pad = l
+        if len(q) != 1:
+            P("-" * max(len(q) + 1, len(p)))
+            for i in q[::-1]:
+                P(str(int(i) * int(p)))
+                l -= 1
+        print("-" * pad)
+        print(e)
+
+    else:
+        P("-" * max(len(q) + 1, len(p)))
+        P(str(e))
+    print()
