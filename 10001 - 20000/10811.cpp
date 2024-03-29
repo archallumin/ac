1ec545ce043f182e5330ac565fec9c12547f6aa8
allumin <windowsfilesystem@gmail.com>
2024-03-29 17:48:05 +0900

    lots of files

--- 10001 - 20000/10811.cpp
+++ 10001 - 20000/10811.cpp
@@ -1,22 +1,24 @@
-#include <iostream>
 #include <algorithm>
+#include <iostream>
 using namespace std;
 
 int main(void) {
-	int n, m;
-	int a, b;
-	int arr[100] = { 0, };
-	cin >> n >> m;
-	for (int i = 1; i <= n; i++) {
-		arr[i] = i;
-	}
-	while (m--) {
-		cin >> a >> b;
-		reverse(&arr[a], &arr[b + 1]);
-	}
-	for (int i = 1; i <= n; i++) {
-		cout << arr[i] << " ";
-	}
-	cout << "\n";
-	return 0;
+  int n, m;
+  int a, b;
+  int arr[100] = {
+      0,
+  };
+  cin >> n >> m;
+  for (int i = 1; i <= n; i++) {
+    arr[i] = i;
+  }
+  while (m--) {
+    cin >> a >> b;
+    reverse(&arr[a], &arr[b + 1]);
+  }
+  for (int i = 1; i <= n; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << "\n";
+  return 0;
 }
--- 10001 - 20000/10818.cpp
+++ 10001 - 20000/10818.cpp
@@ -2,14 +2,16 @@
 using namespace std;
 
 int main(void) {
-	int min = 1000000;
-	int max = -1000000;
-	int a, b;
-	cin >> a;
-	for (int i = 0; i < a; i++) {
-		cin >> b;
-		if (b < min) min = b;
-		else if (b > max) max = b;
-	}
-	cout << min << " " << max;
+  int min = 1000000;
+  int max = -1000000;
+  int a, b;
+  cin >> a;
+  for (int i = 0; i < a; i++) {
+    cin >> b;
+    if (b < min)
+      min = b;
+    else if (b > max)
+      max = b;
+  }
+  cout << min << " " << max;
 }
