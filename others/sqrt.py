6f0b8b96f8d34674b9abb9bd850563216addd8ef
allumin <windowsfilesystem@gmail.com>
2024-04-03 17:29:49 +0900

    kjwrf

--- others/sqrt.py
+++ others/sqrt.py
@@ -4,7 +4,7 @@ import sys
 decimal.getcontext().prec = 60
 t = int(sys.stdin.readline())
 for i in range(1, t + 1):
-    q = int(sys.stdin.readline())
+    q = str(sys.stdin.readline())
 
     print(
         f'Case #{i}: {decimal.getcontext().power(decimal.Decimal(str(q)), decimal.Decimal("0.5"))}'
