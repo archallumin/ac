617768f5297e10f2e237f395f86ab799a3a6b6d1
allumin <windowsfilesystem@gmail.com>
2024-04-08 08:39:28 +0900

    amasmkaskm

--- 5001 - 10000/5565.cpp
+++ 5001 - 10000/5565.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 
 int main(void) {
-	int a, b, total = 0, i = 9;
-	cin >> a;
-	while (i--) {
-		cin >> b;
-		total = total + b;
-	}
-	cout << a - total << "\n";
-	return 0;
+  int a, b, total = 0, i = 9;
+  cin >> a;
+  while (i--) {
+    cin >> b;
+    total = total + b;
+  }
+  cout << a - total << "\n";
+  return 0;
 }
--- 5001 - 10000/5597.cpp
+++ 5001 - 10000/5597.cpp
@@ -2,13 +2,16 @@
 using namespace std;
 
 int main(void) {
-	int a, arr[31] = { 0, };
-	for (int i = 0; i < 28; i++) {
-		cin >> a;
-		arr[a] = 1;
-	}
-	for (int j = 1; j <= 30; j++) {
-		if (!arr[j]) cout << j << "\n";
-	}
-	return 0;
+  int a, arr[31] = {
+             0,
+         };
+  for (int i = 0; i < 28; i++) {
+    cin >> a;
+    arr[a] = 1;
+  }
+  for (int j = 1; j <= 30; j++) {
+    if (!arr[j])
+      cout << j << "\n";
+  }
+  return 0;
 }
