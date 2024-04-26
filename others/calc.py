89f1f3fcef0913e09524f3deb919a95eabf83136
allumin <windowsfilesystem@gmail.com>
2024-04-26 19:44:05 +0900

    jnfrjnfj

--- others/calc.py
+++ others/calc.py
@@ -1,11 +1,3 @@
-cnt = 0
-for i in range(6666666, 66666666):
-    a = i * 15
-    indexcnt = 0
-    for j in range(0, len(str(a))):
-        if int(str(a)[j]) == 1 or int(str(a)[j]) == 5:
-            indexcnt += 1
-        if indexcnt == len(str(a)):
-            print(f"{a} ")
-            cnt += 1
-print(cnt)
+a, b = map(int, input().split())
+a -= 9
+print((a * 60) + b)
