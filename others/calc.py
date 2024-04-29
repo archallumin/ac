87372139304d20ef904347c16589829aa99c39ca
allumin <windowsfilesystem@gmail.com>
2024-04-29 21:16:29 +0900

    l

--- others/calc.py
+++ others/calc.py
@@ -1,11 +1,6 @@
-cnt = 0
-for i in range(6666666, 66666666):
-    a = i * 15
-    indexcnt = 0
-    for j in range(0, len(str(a))):
-        if int(str(a)[j]) == 1 or int(str(a)[j]) == 5:
-            indexcnt += 1
-        if indexcnt == len(str(a)):
-            print(f"{a} ")
-            cnt += 1
-print(cnt)
+import decimal
+a = decimal.Decimal("1")
+decimal.getcontext().prec = 10
+for i in range(2, 10000):
+    a *= decimal.Decimal(str(decimal.Decimal(6+((i ** (1/decimal.Decimal(i ** 3))) + (i * (i - decimal.Decimal(1/12)))))))
+    print(f'{a:.0f}')
