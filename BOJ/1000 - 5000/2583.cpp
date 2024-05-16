234afe7cc93c548fef0452c3f1587e56e9e4e0dc
allumin <windowsfilesystem@gmail.com>
2024-05-16 19:55:52 +0900

    dejn

--- BOJ/1000 - 5000/2583.cpp
+++ BOJ/1000 - 5000/2583.cpp
@@ -3,11 +3,11 @@
 #include <vector>
 using namespace std;
 
-int a;
+int a, b;
 int house_count;
 int house_house_count;
-int arr[26][26];
-int vis[26][26];
+int arr[101][101];
+int vis[101][101];
 vector<int> houses;
 
 int X[4] = {0, 0, 1, -1};
@@ -17,7 +17,7 @@ void dfs(int y, int x) {
   for (int i = 0; i < 4; i++) {
     int xx = x + X[i];
     int yy = y + Y[i];
-    if (xx < 0 || yy < 0 || xx >= a || yy >= a)
+    if (xx < 0 || yy < 0 || xx >= a || yy >= b)
       continue;
     if (!vis[yy][xx] && arr[yy][xx]) {
       vis[yy][xx] = 1;
@@ -28,17 +28,24 @@ void dfs(int y, int x) {
 }
 
 int main(void) {
-  int house_count = 0;
-  cin >> a;
-  for (int i = 0; i < a; i++) {
-    for (int j = 0; j < a; j++) {
-      char str;
-      cin >> str;
-      arr[i][j] = str - '0';
+  int house_count = 0, c;
+  for (int i = 0; i < 101; i++) {
+    for (int j = 0; j < 101; j++) {
+      arr[i][j] = 1;
+    }
+  }
+  cin >> a >> b >> c;
+  while (c--) {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    for (int i = a; i < c; i++) {
+      for (int j = b; j < d; j++) {
+        arr[i][j] = 0;
+      }
     }
   }
   for (int k = 0; k < a; k++) {
-    for (int l = 0; l < a; l++) {
+    for (int l = 0; l < b; l++) {
       if (arr[k][l] && !vis[k][l]) {
         vis[k][l] = 1;
         house_house_count = 1;
