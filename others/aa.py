c8fb13fe2577bd7e0255b47cb9ac44b0e2629cb0
allumin <windowsfilesystem@gmail.com>
2024-05-21 18:19:09 +0900

    frfrfr

--- others/aa.py
+++ others/aa.py
@@ -1,20 +1,20 @@
-from time import time
 from sys import stdin, stdout
-
-start = int(round(time()*1000))
 
 input = stdin.readline
 print = stdout.write
 
-a = int(input())
-b = a//2
+t = int(input())
 
-for i in range(1, a+1):
-  print(("-" * 28) + "+" + ("-" * 28) + "\n")
-  print((f"{i} * n").ljust(57) + "\n")
-  print(("-" * 28) + "+" + ("-" * 28) + "\n")
-  for j in range(1, b+1):
-    print(f"{i} * {j} = {i * j}".ljust(19))
-    print("|".center(19))
-    print(f"{i} * {j + b} = {i * (j + b)}".rjust(19) + "\n")
-print(f"\nTime: {int(round(time()*1000)) - start}ms\n")
+for i in range(t):
+  n, m, b = map(int, input().split())
+  for q in range(n, m + 1):
+    print("+"+("-" * 33) + "+" + ("-" * 33)+"+" + "\n")
+    print((f"| {q} * n").ljust(23))
+    print((f"|").center(23))
+    print((f"(1 ~ {b//2}, {b//2+1} ~ {b}) |").rjust(23) + "\n")
+    print("+"+("-" * 33) + "+" + ("-" * 33)+"+" + "\n")
+    for j in range(1, (b//2)+1):
+      print(f"| {q} * {j} = {q * j}".ljust(25))
+      print("|".center(19))
+      print(f"{q} * {j + b//2} = {q * (j + b//2)} |".rjust(25) + "\n")
+  print("+"+("-" * 33) + "+" + ("-" * 33)+"+" + "\n\n")
