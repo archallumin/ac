75abc2e22de3cbc8b5d6bced3f3d59bb7e3718d6
archallumin <alluminized@naver.com>
2024-10-29 20:04:07 +0900

    sw

--- BOJ/1000 - 5000/2445.cpp
+++ BOJ/1000 - 5000/2445.cpp
@@ -1,3 +1,38 @@
-/**
- * TODO: 나중에 생각해 볼 것
- */
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int a;
+    cin >> a;
+    for (int i=1; i<=a; i++) {
+        for (int j=0; j<i; j++) {
+            cout << "*";
+        }
+        for (int j=0; j<a-i; j++) {
+            cout << " ";
+        }
+        for (int j=0; j<a-i; j++) {
+            cout << " ";
+        }
+        for (int j=0; j<i; j++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    for (int i=a-1; i>=1; i--) {
+        for (int j=0; j<i; j++) {
+            cout << "*";
+        }
+        for (int j=0; j<a-i; j++) {
+            cout << " ";
+        }
+        for (int j=0; j<a-i; j++) {
+            cout << " ";
+        }
+        for (int j=0; j<i; j++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
